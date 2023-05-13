6895629c332a0d437f1582042b3be15f505a26ae
Sowmya Bharathi Pusapati <119491409+sowmya-pusapati@users.noreply.github.com>
2023-05-13 22:15:55 +0530

    Update script.js with code

--- script.js
+++ script.js
@@ -8,20 +8,61 @@ let arr = [
 
 function PrintStudentswithMap() {
   //Write your code here , just console.log
+  arr.map(function(item){
+    if(item.marks>50){
+      console.log(item.name);
+    }
+  });
 }
 
 function PrintStudentsbyForEach() {
   //Write your code here , just console.log
+  arr.forEach(function(item){
+        if(item.marks>50){
+          console.log(item.name);
+        }
+     });
 }
 
 function addData() {
   //Write your code here, just console.log
+  let x={id:4,name:"susan",age:"20",marks:45}
+    arr.push(x);
+    console.log(arr);
 }
 
 function removeFailedStudent() {
   //Write your code here, just console.log
+  let passedStudents = arr.filter((student) => student.marks >= 50);
+arr=passedStudents;
+console.log(arr);
 }
 
 function concatenateArray() {
   //Write your code here, just console.log
+   let sec=
+ [
+    {
+    id:5,
+    name:"mango",
+    age:"18",
+    marks:80
+    }, 
+    {
+    id:4, 
+    name:"goat",
+    age:"20", 
+    marks:85
+    }, 
+    {
+    id:6, 
+    name:"aris",
+     age:"19",
+     marks:35
+    } 
+]
+ let new_arr=arr.concat(sec);  
+ 
+ arr=new_arr;
+ console.log(arr);
 }
